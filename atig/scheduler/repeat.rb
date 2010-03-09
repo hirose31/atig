9609939df6942f02e7eeb79c6a3522dbfbff2107
mzp <mzpppp@gmail.com>
2010-03-09 15:08:02 +0900

    specに合うように実装した

--- atig/scheduler/repeat.rb
+++ atig/scheduler/repeat.rb
@@ -1,14 +1,40 @@
 #! /opt/local/bin/ruby -w
 # -*- mode:ruby; coding:utf-8 -*-
 
+require 'ostruct'
+
 module Atig
   module Scheduler
     class Repeat
-      def init(name, delay); end
-      def with(name, count); end
-      def [](name); 42 end
-      def reset(count, hour); end
+      def initialize
+        @entries = {}
+        @unit    = 1.0
+      end
 
+      def init(name, delay)
+        @entries[name] = OpenStruct.new({
+                                          :ratio => delay,
+                                          :count => 0,
+                                          :api_count => 0
+                                        })
+      end
+
+      def with(name, count)
+        @entries[name].count += 1
+        @entries[name].api_count += count
+      end
+
+      def [](name)
+        @entries[name].ratio * @unit
+      end
+
+      def reset(count, span)
+        @unit = @entries.inject(0){|x,y|
+          _, entry = y
+          estimate = entry.api_count.to_f / entry.count
+          x + estimate / entry.ratio
+        } * span / count
+      end
     end
   end
 end
